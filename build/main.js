021be97f02a33e8fbcaf4f9648fc8ecc09799a12
kylestech95 <kylestech95@outlook.com>
2023-11-24 10:55:04 -0500

    optimized for tablet/mobile view

--- build/main.js
+++ build/main.js
@@ -3,6 +3,8 @@ let body = document.querySelector('body')
 let half_window = body.clientWidth/2
 let half_height = body.clientHeight/2
 let gears = document.querySelectorAll('span')
+let btn = document.querySelector('.btn-press')
+let nav_container = document.querySelector('.nav-container')
 
 //backround appear/disappear mouse event
 window.addEventListener('mousemove',e=>{
@@ -19,4 +21,14 @@ else{
    x.style = `opacity:${quotient}`
    })
 }
+})
+function navFn(){
+   if(nav_container.classList.contains('nav-left')){
+      nav_container.classList.toggle('nav-normal')
+   }
+}
+window.addEventListener('resize',(e)=>{
+   if(e.target.innerWidth >= 715){
+     nav_container.classList.remove('nav-normal')
+   }
 })
