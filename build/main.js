c79b8ceef2d905466c182f89ae8e6c259b7116ce
kylestech95 <kylestech95@outlook.com>
2023-12-18 20:31:51 -0500

    committed

--- build/main.js
+++ build/main.js
@@ -4,17 +4,18 @@ let half_height = body.clientHeight / 2
 let mymouse = { x: undefined, y: undefined };
 let banner = document.getElementById('banner')
 let gears = document.querySelectorAll('span')
-
+let touchContainer = document.getElementById('touch')
+let touchActual = document.getElementById('touch-actual')
 let nav_container = document.querySelector('#nav')
 let tailwind_left = document.querySelector('.li1').getBoundingClientRect().x
 let react_right = document.querySelector('.li5').getBoundingClientRect().x + document.querySelector('.li5').getBoundingClientRect().width
-
+let wh_all = document.querySelectorAll('.wh')
 let section1 = document.querySelector('.flex-control')
 let preview = document.getElementById('preview')
 let preview_daddy = document.querySelector('.preview-container-daddy')
 let preview_arr = document.querySelectorAll('.preview-item')
 let nav_arr = document.querySelectorAll('#nav>.list-container>.list-item')
-let media = ["./media/tailwindrec.mp4","./media/css_log_animation.webm", "./media/form_scroll.webm", "./media/autotxtFn.webm","./media/eightballjs.mp4",]
+let media = ["./media/tailwindrec.mp4", "./media/css_log_animation.webm", "./media/form_scroll.webm", "./media/autotxtFn.webm", "./media/eightballjs.mp4",]
 let scrollDown = document.querySelector('.scroll-down-container');
 let scrollDown2 = document.querySelector('.scroll-down-container2');
 let side = {
@@ -27,37 +28,82 @@ let message0 = document.querySelector('.hidden-message0')
 let message = document.querySelector('.hidden-message')
 let instructions = document.querySelectorAll('.opacity-instructions-container')
 let c = 1;
+let d = 0;
+window.addEventListener('scroll', e => {
+   d++
+   console.log(window.scrollY)
+   // console.log(body.clientHeight)
+   let mid = body.clientHeight/2
+   // console.log(mid)
+   // if(window.scrollY > mid){
+   //    console.log('you hit me')
+   //    wh_all.forEach((header,i) => {
+   //       header.classList.remove('wh-centered-seq')
+   //       if(i%2==0){
+   //          wh_all[i].classList.add('wh-left-seq')
+   //       }
+   //       else{
+   //          wh_all[i].classList.add('wh-right-seq')
 
+   //       }
+   //    })
+   // }
+   // else{
+   //    wh_all.forEach((header,i) => {
+   //       header.classList.add('wh-centered-seq')
+   //       if(i%2==0){
+   //          wh_all[i].classList.remove('wh-left-seq')
+   //       }
+   //       else{
+   //          wh_all[i].classList.remove('wh-right-seq')
+
+   //       }
+   //    })
+   // }
+   wh_all.forEach((head,i)=>{
+      if(i%2==0){
+         head.style=`transform:translate(${window.scrollY}px,0);`
+      }
+      else{
+         head.style=`transform:translate(${window.scrollY*-1}px,0);`
+
+      }
+   })
+
+})
 //floating name of category to choose from
-let float = ()=> {
+let float = () => {
    message0.classList.add('float-up')
 }
-document.querySelectorAll('*').forEach(element=>element.setAttribute('notab','notab'))
+document.querySelectorAll('*').forEach(element => element.setAttribute('notab', 'notab'))
 let section = document.querySelector('.blank-container')
 
+wh_all.forEach(header => {
+   header.classList.add('wh-centered-seq')
+})
 
 //Keydown that detects the "Tab" key & disables every element when pressing "Tab"
-window.addEventListener('keydown',e=>{
-   if(e.key==='Tab'){
+window.addEventListener('keydown', e => {
+   if (e.key === 'Tab') {
       e.preventDefault()
    }
-   
+
 })
-instructions.forEach((ins,i)=>{
-  
-   setTimeout(()=>{
-      ins.style.opacity='0'
-   },8000)
-   setTimeout(()=>{
-      if(i===0){
+instructions.forEach((ins, i) => {
+
+   setTimeout(() => {
+      ins.style.opacity = '0'
+   }, 8000)
+   setTimeout(() => {
+      if (i === 0) {
          ins.classList.remove('-left-40')
          ins.classList.add('left-24')
       }
-      else{
+      else {
          ins.classList.remove('-right-40')
          ins.classList.add('right-24')
       }
-   },750*(i+1))
+   }, 750 * (i + 1))
 })
 
 //Function list
@@ -65,10 +111,10 @@ instructions.forEach((ins,i)=>{
 $(document).ready(function () {
    $(this).scrollTop(0)
 });
-function noPointer(){
+function noPointer() {
    body.classList.add('pointer-events-none')
 }
-function hiPointer(){
+function hiPointer() {
    body.classList.remove('pointer-events-none')
 }
 function autoTextFn(text, heading) {
@@ -76,21 +122,21 @@ function autoTextFn(text, heading) {
    text = [...text]//text.split``
    let i = 0, arr = [], len = text.length
    let timer = setInterval(() => {
-     let take = text.shift(text[i])
-     i += 1
-     arr.push(take)
-     heading.textContent = arr.join``
-     // console.log(text)//sender
-     // console.log(arr)//receiver
-     // console.log(arr.length,len)//compare arr's length w/ original text length
-     if (arr.length === len) {
-      clearInterval(timer)
-      hiPointer()
-   }//clearInterval once both lengths are the same.
+      let take = text.shift(text[i])
+      i += 1
+      arr.push(take)
+      heading.textContent = arr.join``
+      // console.log(text)//sender
+      // console.log(arr)//receiver
+      // console.log(arr.length,len)//compare arr's length w/ original text length
+      if (arr.length === len) {
+         clearInterval(timer)
+         hiPointer()
+      }//clearInterval once both lengths are the same.
    }, 50)
 }
 
-function previewAppear(){
+function previewAppear() {
    preview.classList.remove('opacity-0')
    preview.classList.add('opacity-1')
    preview.classList.remove('pointer-events-none')
@@ -99,32 +145,32 @@ function previewAppear(){
    preview.children[0].classList.remove('opacity-0')
    return
 }
-function hiddenMessageAppear(){
-   setTimeout(float,250)
+function hiddenMessageAppear() {
+   setTimeout(float, 250)
    message0.classList.remove('message-gone')
    message0.classList.add('message-appear')
    message.classList.remove('message-gone')
    message.classList.add('message-appear')
 }
-function bannerAppear(){
+function bannerAppear() {
    banner.classList.remove('opacity-1')
    banner.classList.add('opacity-0')
    banner.classList.add('pointer-events-none')
    return
 }
-function scrollDownDisappear(){
+function scrollDownDisappear() {
    scrollDown.classList.remove('opacity-1')
    scrollDown.classList.add('opacity-0')
    scrollDown.classList.add('pointer-events-none')
    return
 }
-function scrollDownDisappear2(){
+function scrollDownDisappear2() {
    scrollDown2.classList.remove('opacity-1')
    scrollDown2.classList.add('opacity-0')
    scrollDown2.classList.add('pointer-events-none')
    return
 }
-function previewDisappear(){
+function previewDisappear() {
    preview.classList.remove('opacity-1')
    preview.classList.add('opacity-0')
    preview.classList.add('pointer-events-none')
@@ -134,7 +180,7 @@ function previewDisappear(){
    preview.classList.remove('shadow-2xl')
    return
 }
-function hiddenMessageDisappear(){
+function hiddenMessageDisappear() {
    clearTimeout(float)
    message0.classList.remove('float-up')
    message0.classList.remove('message-appear')
@@ -142,45 +188,46 @@ function hiddenMessageDisappear(){
    message.classList.remove('message-appear')
    message.classList.add('message-gone')
 }
-function bannerDisappear(){
+function bannerDisappear() {
    banner.classList.remove('pointer-events-none')
    banner.classList.remove('opacity-0')
    banner.classList.add('opacity-1')
    return
 }
-function scrollDownAppear(){
+function scrollDownAppear() {
    scrollDown.classList.add('opacity-1')
    scrollDown.classList.remove('opacity-0')
    scrollDown.classList.remove('pointer-events-none')
    return
 }
-function scrollDownAppear2(){
+function scrollDownAppear2() {
    scrollDown2.classList.add('opacity-1')
    scrollDown2.classList.remove('opacity-0')
    scrollDown2.classList.remove('pointer-events-none')
    return
 }
-function scrollDownFn(){
+function scrollDownFn() {
    // let target = section.getBoundingClientRect().y;
    let target = body.clientHeight;
-   window.scrollTo(0,target);
-   
+   window.scrollTo(0, target);
+
+
 }
-function scrollUpFn(){
+function scrollUpFn() {
    // let target = section.getBoundingClientRect().y;
    let target = body.clientHeight;
-   window.scrollTo(0,0);
-   
+   window.scrollTo(0, 0);
+
 }
 function appear() {
-bannerAppear()
-hiddenMessageAppear()
-previewAppear()
+   bannerAppear()
+   hiddenMessageAppear()
+   previewAppear()
 }
 function disappear() {
-previewDisappear()
-hiddenMessageDisappear()
-bannerDisappear()
+   previewDisappear()
+   hiddenMessageDisappear()
+   bannerDisappear()
 }
 function mouse_over_out() {
    let caption = document.createElement('h1')
@@ -200,7 +247,7 @@ function mouse_over_out() {
          caption.classList.add('figCap')
          // caption.classList.add('text-gray-500')
          caption.classList.add('text-red-500')
-         
+
          let source = e.currentTarget.children[0]
          if (e.currentTarget.paused || e.currentTarget.played.length === 0) { //play e.currentTarget if it has not started
             e.currentTarget.muted = true
@@ -217,9 +264,9 @@ function mouse_over_out() {
          message.classList.remove('message-gone')
          message0.classList.add('message-appear')
          message0.classList.remove('message-gone')
-         if(e.currentTarget.played.length > 0){
+         if (e.currentTarget.played.length > 0) {
             e.currentTarget.pause()
-            
+
          }
       })
    })
@@ -234,15 +281,15 @@ function navFn() {
       bannerAppear();
    }
    if (!/nav-center/g.test(nav_container.classList.value)) {
-         animation.style = "animation:peek 1s ease-in-out infinite alternate";
-         animation.style.left = `-125px`;
-         bannerDisappear()
+      animation.style = "animation:peek 1s ease-in-out infinite alternate";
+      animation.style.left = `-125px`;
+      bannerDisappear()
    }
 }
-
+let acc = 0;
 //backround appear/disappear mouse event
 window.addEventListener('mousemove', e => {
-e.preventDefault()
+   e.preventDefault()
    mymouse = { x: e.pageX, y: e.pageY }
    let quotient = mymouse.x / 1000
 
@@ -258,7 +305,10 @@ e.preventDefault()
          x.style = `opacity:${quotient}`
       })
    }
-
+   //unique Touch animation
+   if((mymouse.x < touchContainer.getBoundingClientRect().x+touchContainer.getBoundingClientRect().width && mymouse.x > touchContainer.getBoundingClientRect().x)&&(mymouse.y < touchContainer.getBoundingClientRect().y+touchContainer.getBoundingClientRect().height && mymouse.y > touchContainer.getBoundingClientRect().y)){
+      touchActual.style=`position:absolute;transform:translate(${acc++}px,0);`
+   }
    //if the user moves their mouse outside of the preview pane and/or nav-items(html or Js)
    if (body.clientWidth > 600) {
       if (preview.classList.contains('opacity-1') && ((mymouse.x < side.left || mymouse.x > side.right) || (mymouse.y < 105 && (mymouse.x < tailwind_left || mymouse.x > react_right)))) {
@@ -270,7 +320,7 @@ e.preventDefault()
          preview_arr.forEach((item, i) => {
             let video = item.children[0].children[0]
             let source = video.children[0]
-         
+
          })
       }
 
@@ -281,18 +331,18 @@ e.preventDefault()
    }
 })
 //resize window listener
-window.addEventListener('resize', e =>{
-let myWidth = e.target.innerWidth;
-let arr = [...nav_container.children[0].children]
+window.addEventListener('resize', e => {
+   let myWidth = e.target.innerWidth;
+   let arr = [...nav_container.children[0].children]
 })
-function pressed(li){
-li.classList.add('pressed')
+function pressed(li) {
+   li.classList.add('pressed')
 }
 //remove all that is pressed
-function unpressed(){
-nav_arr.forEach(li=>{
-   li.classList.remove('pressed')
-})
+function unpressed() {
+   nav_arr.forEach(li => {
+      li.classList.remove('pressed')
+   })
 }
 //assigning video sources depending on nav-list-item
 nav_arr.forEach((li, i) => {
@@ -316,29 +366,4 @@ nav_arr.forEach((li, i) => {
 //disappearing video on mouseout
 mouse_over_out()
 
-
-// //render data into html
-// function render(d){
-// let data = JSON.parse(d.target.responseText).html_data
-
-// //forEach loop on preview-tiles
-// preview_href.forEach((src,i)=>{
-//    //target <a></a> tag (parent)
-//    src.href=data[0].link; //testing
-//    src.target=data[0].target//testing
-// })
-// }
-
-// //upload json
-// let xml = new XMLHttpRequest();
-// let url='./mydata/html.json';
-// let method = 'GET';
-// //open xml
-// xml.open(method,url,false)//false for async operations
-
-// //xml.onload
-// xml.onload=(d)=>{
-//    JSON.parse(d.target.response)
-//    render(d)
-// }
-// xml.send();
+//touch event "touch"
